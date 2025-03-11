244047f2d36273db6e137f55a2ef925d346b575b
onyevyerov <onyevyerov@gmail.com>
2025-03-11 16:33:07 +0100

    feat(cinema):create Actor and Genre  models

--- theatre/models.py
+++ theatre/models.py
@@ -4,9 +4,19 @@ from django.db import models
 class Play(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
+    actors = models.ManyToManyField("Actor", related_name="plays", blank=True)
+    genres = models.ManyToManyField("Genre", related_name="plays", blank=True)
 
     def __str__(self):
         return self.title
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 
 class TheatreHall(models.Model):
@@ -25,3 +35,5 @@ class Performance(models.Model):
 
     def __str__(self):
         return f"{self.play.title} in {self.theatre_hall.name} at {self.show_time}"
+
+
