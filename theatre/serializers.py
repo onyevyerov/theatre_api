09c70ed638687024fd9e34df33244496079c23a2
onyevyerov <onyevyerov@gmail.com>
2025-03-11 16:48:23 +0100

    feat(theatre):create TheatreHall serializer

--- theatre/serializers.py
+++ theatre/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from theatre.models import Actor, Genre
+from theatre.models import Actor, Genre, TheatreHall
 
 
 class ActorSerializer(serializers.ModelSerializer):
@@ -13,3 +13,9 @@ class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
         fields = ("id", "name")
+
+
+class TheatreHallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TheatreHall
+        fields = ("id", "name", "rows", "seats_in_row")
