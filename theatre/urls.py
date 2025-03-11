7c739c664be5c2af3b68a750053dcac95410602b
onyevyerov <onyevyerov@gmail.com>
2025-03-11 18:24:03 +0100

    feat(theatre):create URLs and  CRUD views for Actor, Genre, TheatreHall and Play models

--- theatre/urls.py
+++ theatre/urls.py
@@ -1,5 +1,17 @@
 from django.urls import path, include
+from rest_framework import routers
+
+from theatre.views import ActorViewSet, GenreViewSet, TheatreHallViewSet, PlayViewSet
 
 app_name = "theatre"
 
-urlpatterns = []
+
+router = routers.DefaultRouter()
+router.register("actors", ActorViewSet)
+router.register("genres", GenreViewSet)
+router.register("theatre-halls", TheatreHallViewSet)
+router.register("plays", PlayViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
--- theatre/views.py
+++ theatre/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from theatre.models import Actor, Genre, TheatreHall, Play
+from theatre.serializers import ActorSerializer, GenreSerializer, TheatreHallSerializer, PlaySerializer
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class TheatreHallViewSet(viewsets.ModelViewSet):
+    queryset = TheatreHall.objects.all()
+    serializer_class = TheatreHallSerializer
+
+
+class PlayViewSet(viewsets.ModelViewSet):
+    queryset = Play.objects.all()
+    serializer_class = PlaySerializer
+
