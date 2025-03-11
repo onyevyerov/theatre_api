ac5f740aa5e29ce6f889d46db32545a2ce3d227c
onyevyerov <onyevyerov@gmail.com>
2025-03-11 16:46:08 +0100

    feat(theatre):create Actor serializer

---
+++ theatre/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from theatre.models import Actor
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = ("id", "first_name", "last_name")
