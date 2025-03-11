33288c564c8ec4fde575756e6c34ba3dfa17dc87
onyevyerov <onyevyerov@gmail.com>
2025-03-11 16:28:18 +0100

    feat(cinema):create Perfomance model

--- theatre/models.py
+++ theatre/models.py
@@ -16,3 +16,12 @@ class TheatreHall(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Performance(models.Model):
+    play = models.ForeignKey(Play, on_delete=models.CASCADE, related_name='performances')
+    theatre_hall = models.ForeignKey(TheatreHall, on_delete=models.CASCADE, related_name='performances')
+    show_time = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.play.title} in {self.theatre_hall.name} at {self.show_time}"
