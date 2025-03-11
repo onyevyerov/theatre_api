db042786608007d20a6ecde8405a5c2fcdd31406
onyevyerov <onyevyerov@gmail.com>
2025-03-11 16:46:32 +0100

    feat(theatre):create Genre serializer

--- theatre/serializers.py
+++ theatre/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from theatre.models import Actor
+from theatre.models import Actor, Genre
 
 
 class ActorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Actor
         fields = ("id", "first_name", "last_name")
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ("id", "name")
