6306d42c5808bff0c5961fc512c63a7d2786afca
onyevyerov <onyevyerov@gmail.com>
2025-03-13 13:44:35 +0100

    feat(theatre): add validation for Ticket model

--- theatre/models.py
+++ theatre/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from theatre_service import settings
@@ -60,8 +61,47 @@ class Ticket(models.Model):
     performance = models.ForeignKey(Performance, on_delete=models.CASCADE, related_name='tickets')
     reservation = models.ForeignKey("Reservation", on_delete=models.CASCADE, related_name='tickets')
 
+    @staticmethod
+    def validate_ticket(row, seat, theatre_hall, error_to_raise):
+        for ticket_attr_value, ticket_attr_name, theatre_hall_attr_name in [
+            (row, "row", "rows"),
+            (seat, "seat", "seats_in_row"),
+        ]:
+            count_attrs = getattr(theatre_hall, theatre_hall_attr_name)
+            if not (1 <= ticket_attr_value <= count_attrs):
+                raise error_to_raise(
+                    {
+                        ticket_attr_name: f"{ticket_attr_name} "
+                        f"number must be in available range: "
+                        f"(1, {theatre_hall_attr_name}): "
+                        f"(1, {count_attrs})"
+                    }
+                )
+
+    def clean(self):
+        Ticket.validate_ticket(
+            self.row,
+            self.seat,
+            self.performance.theatre_hall,
+            ValidationError,
+        )
+
+    def save(
+        self,
+        force_insert=False,
+        force_update=False,
+        using=None,
+        update_fields=None,
+    ):
+        self.full_clean()
+
+        return super(Ticket, self).save(force_insert, force_update, using, update_fields)
+
     def __str__(self):
-        return f"{self.row} {self.seat} {self.performance}"
+        return f"{str(self.performance)} (seat: {self.seat}, row: {self.row}) "
+
+    class Meta:
+        unique_together = ("performance", "row", "seat")
 
 
 class Reservation(models.Model):
