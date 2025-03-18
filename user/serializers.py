84af1ce15fed906d02493206fa338ca623bc7bda
onyevyerov <onyevyerov@gmail.com>
2025-03-18 15:04:49 +0100

    fix(user): made password field data not visible

--- user/serializers.py
+++ user/serializers.py
@@ -3,17 +3,17 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        style={'input_type': 'password'},
+        min_length=8,
+    )
 
     class Meta:
         model = get_user_model()
         fields = ("id", "username", "email", "password", "is_staff")
         read_only_fields = ("is_staff",)
-        extra_kwargs = {
-            "password": {
-                "write_only": True,
-                "min_length": 8,
-            }
-        }
 
     def create(self, validated_data):
         """Create a new user with encrypted password and return it"""
