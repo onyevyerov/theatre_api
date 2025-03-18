7777bd813982a054d5609f84f2dd093222b2cfe1
onyevyerov <onyevyerov@gmail.com>
2025-03-18 15:04:49 +0100

    fix(user): made password field data not visible

--- theatre_service/urls.py
+++ theatre_service/urls.py
@@ -24,6 +24,7 @@ from theatre_service import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/theatre/", include("theatre.urls", namespace="theatre")),
+    path("api/user/", include("user.urls", namespace="user")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
