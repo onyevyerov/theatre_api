33fdad7649235a4a61e9e40bae09eeef6866bcb8
onyevyerov <onyevyerov@gmail.com>
2025-03-20 00:22:45 +0100

    feat(user): changed username on email in login and register

--- user/serializers.py
+++ user/serializers.py
@@ -1,19 +1,23 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
+from django.utils.translation import gettext as _
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
+        label=_("Password"),
+        style={"input_type": "password"},
+        trim_whitespace=False,
         write_only=True,
-        required=True,
-        style={'input_type': 'password'},
-        min_length=8,
+        min_length=6,
     )
 
     class Meta:
         model = get_user_model()
-        fields = ("id", "username", "email", "password", "is_staff")
+        fields = ("id", "email", "password", "is_staff")
         read_only_fields = ("is_staff",)
+        extra_kwargs = {"password": {"write_only": True, "min_length": 6}}
 
     def create(self, validated_data):
         """Create a new user with encrypted password and return it"""
@@ -21,9 +25,47 @@ class UserSerializer(serializers.ModelSerializer):
 
     def update(self, instance, validated_data):
         """Update a user, set the password correctly and return it"""
-        password = validated_data.pop('password', None)
+        password = validated_data.pop("password", None)
         user = super().update(instance, validated_data)
         if password:
             user.set_password(password)
             user.save()
+
         return user
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    email = serializers.CharField(label=_("Email address"), write_only=True)
+    password = serializers.CharField(
+        label=_("Password"),
+        style={"input_type": "password"},
+        trim_whitespace=False,
+        write_only=True,
+        min_length=6,
+    )
+    access = serializers.CharField(label=_("Access Token"), read_only=True)
+    refresh = serializers.CharField(label=_("Refresh Token"), read_only=True)
+
+    def validate(self, attrs):
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        if not email or not password:
+            raise serializers.ValidationError(_('Must include "email" and "password".'), code="authorization")
+
+        user = authenticate(request=self.context.get("request"), username=email, password=password)
+
+        if not user:
+            raise serializers.ValidationError(_("Unable to log in with provided credentials."), code="authorization")
+
+        if not user.is_active:
+            raise serializers.ValidationError(_("User account is disabled."), code="authorization")
+
+        # Generate JWT tokens
+        refresh = RefreshToken.for_user(user)
+
+        return {
+            "user": user,
+            "access": str(refresh.access_token),
+            "refresh": str(refresh),
+        }
