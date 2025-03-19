f4fe0b179b53a4df0c54cb2b306129356c8b1b26
onyevyerov <onyevyerov@gmail.com>
2025-03-19 14:31:02 +0100

    feat(user): make possible to create account to everyone

--- user/views.py
+++ user/views.py
@@ -1,14 +1,19 @@
 from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from user.serializers import UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
     def get_object(self):
         return self.request.user
